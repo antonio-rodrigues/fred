9dae434b0d67f3ac1b2f9b9f77cab230db38a8d0
Antonio Rodrigues <antonio.rodrigues@b-i.com>
2017-10-20 17:14:03 +0100

    Add zoom and pan to map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
+import { timeout } from 'rxjs/operator/timeout';
 import { Observer } from 'rxjs/Rx';
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit } from '@angular/core';
 
 import { Logger } from '../core/logger.service';
 import { MapService } from './map.service';
@@ -21,9 +22,18 @@ export class MapComponent implements OnInit {
   /* */
   isLoading: boolean;
   version: string = environment.version;
-  mapElement: string = 'world'
+  mapAnchor: string = '#map-holder'
 
-  // d3 vars
+  // Define size of map group
+  // Full world map is 2:1 ratio; using 12:5 because we will crop top and bottom of map
+  private metrics = {
+    w: 1250,
+    h: 720,
+    minZoom: 1,
+    maxZoom: 4,
+    scale: 140,
+    duration: 500
+  }
   private d3: D3;
   private parentNativeElement: any;
   private d3map: Selection<any, any, any, any>;
@@ -31,25 +41,26 @@ export class MapComponent implements OnInit {
   private group: Selection<SVGGElement, any, null, undefined>;
   private projection: any;
   private path: any;
+  private centered: any = null;
   // d3 map data
   private countryData: any;
   private countryNames: any;
-  // d3 map config
-  private scale: number = 140;
-  private width: number = 1250; // TODO: calc()
-  private height: number = 700; // TODO: calc()
-  private centered: any = null;
-  private minZoom: number = 1; // default 1:1 zoom
-  private maxZoom: number = 4;
-  private transitionDuration: number = 500;
 
   constructor(
+    ngZone: NgZone,
     element: ElementRef,
     d3Service: D3Service,
     private mapService: MapService
   ) {
     this.d3 = d3Service.getD3();
-    this.parentNativeElement = element.nativeElement;
+    this.parentNativeElement = element //.nativeElement;
+    window.onresize = (e) => {
+      log.debug('> window.onresize()');
+      ngZone.run(() => {
+        this.metrics.w = this.getMetrics().w;
+        this.metrics.h = this.getMetrics().h;
+      });
+    };
   }
 
   ngOnInit() {
@@ -58,16 +69,20 @@ export class MapComponent implements OnInit {
     this.mapService.getCountryNames()
     .subscribe((data: any) => {
       this.countryNames = data;
-      // log.info({ _countryNames: this.countryNames })
+      log.info({ service_countryNames: this.countryNames })
     });
     // load topology
     this.mapService.getCountriesTopology()
       .finally(() => { this.isLoading = false; })
       .subscribe((data: any) => {
         this.countryData = data;
-        // log.info({ _countryData: this.countryData })
+        log.info({ service_countryData: this.countryData })
         this.buildMap();
     });
+  }
+
+  ngAfterContentInit() {
+    this.setMetrics();
   }
 
   ngOnDestroy() {
@@ -77,13 +92,41 @@ export class MapComponent implements OnInit {
     }
   }
 
+  getMetrics() {
+    log.info('> getMetrics()')
+    if (this.parentNativeElement.nativeElement && this.parentNativeElement.nativeElement.offsetParent) {
+      return {
+        w: this.parentNativeElement.nativeElement.offsetParent.clientWidth || this.metrics.w,
+        h: this.parentNativeElement.nativeElement.offsetParent.clientHeight || this.metrics.h
+      };
+    } else {
+      return {
+        w: this.metrics.w,
+        h: this.metrics.h
+      };
+    }
+  }
+
+  setMetrics() {
+    log.info('> setMetrics()')
+    let t = setTimeout(() => {
+      if (this.parentNativeElement.nativeElement && this.parentNativeElement.nativeElement.offsetParent) {
+        this.metrics.w = this.parentNativeElement.nativeElement.offsetParent.clientWidth || this.metrics.w;
+        this.metrics.h = this.parentNativeElement.nativeElement.offsetParent.clientHeight || this.metrics.h;
+        log.debug({ metrics: this.metrics });
+      }
+    }, 1000);
+  }
+
   /**
    * D3 interactive World Map
    */
   buildMap() {
     /* */
     const d3 = this.d3;
+    let topology: any[] = null;
 
+    /*
     const onMouseMove = (d: any) => {
       // TODO: add Country Info tooltips...
       // log.info('> mouse move on:', d.name)
@@ -98,12 +141,12 @@ export class MapComponent implements OnInit {
         var centroid = this.path.centroid(d);
         x = centroid[0];
         y = centroid[1];
-        k = this.maxZoom;
+        k = this.metrics.maxZoom;
         this.centered = d;
       } else {
-        x = this.width / 2;
-        y = this.height / 2;
-        k = this.minZoom;
+        x = this.metrics.w / 2;
+        y = this.metrics.h / 2;
+        k = this.metrics.minZoom;
         this.centered = null;
       }
 
@@ -111,39 +154,56 @@ export class MapComponent implements OnInit {
         .classed("active", this.centered && function(d: any) { return d === this.centered; });
 
       this.group.transition()
-        .duration(this.transitionDuration)
-        .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+        .duration(this.metrics.duration)
+        .attr("transform", "translate(" + this.metrics.w / 2 + "," + this.metrics.h / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
         .style("stroke-width", 1.5 / k + "px");
 
       this.svg.selectAll("text").transition()
-        .duration(this.transitionDuration)
+        .duration(this.metrics.duration)
         .attr("transform", (d: any) => { return "translate(" + this.path.centroid(d) + ")scale(" + k + ")translate(" + -d.x + "," + -d.y + ")"})
-        // .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")scale(" + k + ")translate(" + -d.x + "," + -d.y + ")")
+        // .attr("transform", "translate(" + this.metrics.w / 2 + "," + this.metrics.h / 2 + ")scale(" + k + ")translate(" + -d.x + "," + -d.y + ")")
+    }
+    */
+
+    const appendCountryNames = () => {
+      // map names to countries topology
+      const countryNamesParsed = d3.tsvParse(this.countryNames._body);
+      topology.filter((d) => {
+        return countryNamesParsed.some((n: any) => {
+          if (d.id == n.id) return d.name = n.name;
+        });
+      }).sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+      return topology;
     }
 
-    // do we have a placehlder
+    // do we have a placeholder
     if (this.parentNativeElement !== null) {
-      // get root
-      this.d3map = d3.select('#' + this.mapElement);
+      console.group('BUILD_WORLD_MAP')
+
+      // get topology
+      topology = topojson.feature(this.countryData, this.countryData.objects.countries).features;
+      topology = appendCountryNames();
+      log.debug({ topology });
+
+      // get root element
+      this.d3map = d3.select(this.mapAnchor);
 
       // define map projection
-      let _projectionMap = this.projection = d3
-        .geoMercator()
-        .scale(this.width / ( 2 * Math.PI)) // scale to fit group width
-        .translate([this.width / 2, this.height / 1.45]); // ensure centered in group
+      let _projectionMap = this.projection = d3.geoMercator()
+        .scale(this.metrics.w / ( 2.4 * Math.PI)) // scale to fit group width
+        // .scale(this.metrics.scale)
+        .translate([this.metrics.w / 2, this.metrics.h / 1.45]); // ensure centered in group
 
       // define map path
       this.path = d3.geoPath().projection(this.projection);
 
-      // apply zoom to countriesGroup
+      // apply zoom to group
       const zoomed = () => {
         let t = d3.event.transform;
-        this.group.attr(
-          "transform",
-          "translate(" + [t.x, t.y] + ")scale(" + t.k + ")"
-        );
+        this.group.attr("transform", "translate(" + [t.x, t.y] + ")scale(" + t.k + ")");
       };
-      // set zoom behaviour
       var zoom = d3.zoom().on("zoom", zoomed);
 
       // bbox: contains the bounding box of a selected object
@@ -153,95 +213,172 @@ export class MapComponent implements OnInit {
         });
       }
 
-      this.d3map.append('svg')
-
-      this.svg = this.d3map.select<SVGSVGElement>('svg');
-      this.svg
-        .attr('width', this.width)
-        .attr('height', this.height)
-        // .style("stroke", Themes.WorldMap.color5)
-        // .style("fill", Themes.WorldMap.color4)
-        // .style("stroke-width", 1)
-        .attr("preserveAspectRatio", "xMidYMid");
-
-      const group = this.group = this.svg.append<SVGGElement>('g');
-
-      // get countries topology
-      // const countries = topojson.feature(this.countryData, this.countryData.objects.countries).features;
-      // const land = topojson.feature(this.countryData, this.countryData.objects.land);
-      const countries = topojson.feature(this.countryData, this.countryData.objects.countries).features;
-      // const borders = topojson.mesh(this.countryData, this.countryData.objects.countries, (a: any, b: any) => { return a !== b; });
-
-      // map names to countries topology
-      const countryNamesParsed = d3.tsvParse(this.countryNames._body)
-      countries.filter((d) => {
-        return countryNamesParsed.some((n: any) => {
-          if (d.id == n.id) return d.name = n.name;
-        });
-      }).sort((a, b) => {
-        return a.name.localeCompare(b.name);
-      });
-      log.debug({_countries: countries })
-
-      const appendLabels = (d: any, i: number) => {
-        const labels = this.svg.append("text")
-          .attr("x", this.path)
-          .attr("dy", 18);
-        labels.append("textPath")
-          .attr("stroke","silver")
-          .attr("fill","silver")
-          .attr("xlink:href","#path" + i)
-          .text(d.count);
+      // calculates zoom/pan limits and sets zoom to default value
+      const initiateZoom = () => {
+        log.debug('> initiateZoom:', this.getMetrics())
+        // Define a "minzoom" whereby the "Countries" is as small possible without leaving white space at top/bottom or sides
+        const minZoom = Math.max(this.getMetrics().w / this.metrics.w, this.getMetrics().h / this.metrics.h);
+        // set max zoom to a suitable factor of this value
+        const maxZoom = Math.round(20 * minZoom);
+        // set extent of zoom to chosen values
+        // set translate extent so that panning can't cause map to move out of viewport
+        zoom
+          .scaleExtent([minZoom, maxZoom])
+          .translateExtent([[0, 0], [this.metrics.w, this.metrics.h]]) // set extent of panning
+        // update config
+        this.metrics.minZoom = minZoom;
+        this.metrics.maxZoom = maxZoom;
+        // define X and Y offset for centre of map
+        const midX = (this.getMetrics().w - (minZoom * this.metrics.w)) / 2;
+        const midY = (this.getMetrics().h - (minZoom * this.metrics.h)) / 2;
+        // change zoom transform to min zoom and centre offsets
+        this.svg.call(zoom.transform, d3.zoomIdentity.translate(midX, midY).scale(minZoom));
       }
 
-      // add country paths
-      group.selectAll('path')
-        .data(countries)
+      // zoom to show a bounding box, with optional additional padding as percentage of box size
+      const boxZoom = (box: any, centroid: any, paddingPerc: any) => {
+        let minXY = box[0];
+        let maxXY = box[1];
+        // find size of map area defined
+        let zoomWidth = Math.abs(minXY[0] - maxXY[0]);
+        let zoomHeight = Math.abs(minXY[1] - maxXY[1]);
+        // find midpoint of map area defined
+        let zoomMidX = centroid[0];
+        let zoomMidY = centroid[1];
+        // increase map area to include padding
+        zoomWidth = zoomWidth * (1 + paddingPerc / 100);
+        zoomHeight = zoomHeight * (1 + paddingPerc / 100);
+        // find scale required for area to fill svg
+        let maxXscale = this.metrics.w / zoomWidth;
+        let maxYscale = this.metrics.h / zoomHeight;
+        let zoomScale = Math.min(maxXscale, maxYscale);
+        // handle some edge cases
+        // limit to max zoom (handles tiny countries)
+        zoomScale = Math.min(zoomScale, this.metrics.maxZoom);
+        // limit to min zoom (handles large countries and countries that span the date line)
+        zoomScale = Math.max(zoomScale, this.metrics.minZoom);
+        // Find screen pixel equivalent once scaled
+        let offsetX = zoomScale * zoomMidX;
+        let offsetY = zoomScale * zoomMidY;
+        // Find offset to centre, making sure no gap at left or top of holder
+        let dleft = Math.min(0, this.getMetrics().w / 2 - offsetX);
+        let dtop = Math.min(0, this.getMetrics().h / 2 - offsetY);
+        // Make sure no gap at bottom or right of holder
+        dleft = Math.max(this.getMetrics().w - this.metrics.w * zoomScale, dleft);
+        dtop = Math.max(this.getMetrics().h - this.metrics.h * zoomScale, dtop);
+        // set zoom
+        this.svg
+          .transition()
+          .duration(500)
+          .call(
+            zoom.transform,
+            d3.zoomIdentity.translate(dleft, dtop).scale(zoomScale)
+          );
+      }
+
+
+
+      // base SVG obj
+      this.d3map.append('svg')
+      this.svg = this.d3map.select<SVGSVGElement>('svg');
+      this.svg
+        .attr('width', this.metrics.w)
+        .attr('height', this.metrics.h)
+        .call(zoom);
+
+      // countries group
+      const group = this.group = this.svg.append<SVGGElement>('g')
+        .attr("id", "map");
+      // add a background rectangle
+      group
+        .append("rect")
+        .attr("fill", "none")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", this.metrics.w)
+        .attr("height", this.metrics.h);
+
+      // draw a path for each feature/country
+      let countriesGroup = this.group
+        .selectAll("path")
+        .data(topology)
         .enter()
-        .append('path')
-        .attr('d', this.path)
-        .style("fill", Themes.WorldMap.color8) // function(d) { return color(namesById[d.id]); })
-        .style('stroke', 'white')
-        .style('stroke-width', 1.5)
-        .style("opacity", 0.8)
-        .on("click", onClicked)
-        .on('mousemove', onMouseMove);
+        .append("path")
+        .attr("d", this.path)
+        .style("fill", Themes.WorldMap.color8)
+        .attr("id", (d, i) => {
+          return "country" + d.id;
+        })
+        .attr("class", "country")
+        // add a mouseover action to show name label for feature/country
+        .on("mouseover", function(d, i) {
+          log.info('d', d.name)
+          d3.select("#countryLabel" + d.id).style("display", "block");
+        })
+        .on("mouseout", function(d, i) {
+          d3.select("#countryLabel" + d.id).style("display", "none");
+        })
+        // add an onclick action to zoom into clicked country
+        .on("click", (d: any, i: number) => {
+          // d3.selectAll(".country").classed("country-on", false);
+          // d3.select(d).classed("country-on", true);
+          boxZoom(this.path.bounds(d), this.path.centroid(d), 20);
+        });
 
-        //Add circles to the svgContainer
-        var circles = this.svg.selectAll("circle")
-                           .data(countries)
-                           .enter()
-                           .append("circle");
+      let countryLabels = countriesGroup
+        .selectAll("g")
+        .data(topology)
+        .enter()
+        .append("g")
+        .attr("class", "countryLabel")
+        .attr("id", (d) => {
+          return "countryLabel" + d.id;
+        })
+        .attr("transform", (d: any) => {
+          return (
+            "translate(" + this.path.centroid(d)[0] + "," + this.path.centroid(d)[1] + ")"
+          );
+        })
+        // // add mouseover functionality to the label
+        // .on("mouseover", function(d, i) {
+        //   log.info('d', d.name)
+        //    d3.select(this).style("display", "block");
+        // })
+        // .on("mouseout", function(d, i) {
+        //     d3.select(this).style("display", "none");
+        // })
+        // add an onclick action to zoom into clicked country
+        // .on("click", (d: any, i: any) => {
+        //    d3.selectAll(".country").classed("country-on", false);
+        //    d3.select("#country" + d.properties.iso_a3).classed("country-on", true);
+        //    boxZoom(this.path.bounds(d), this.path.centroid(d), 20);
+        // });
 
-        //Add the circle attributes
-        var circleAttributes = circles
-                       .attr("cx", (d: any) => { return d.cx; })
-                       .attr("cy", (d: any) => { return d.cy; })
-                       .attr("r", (d: any) => { return d.radius; })
-                       .style("fill", (d: any) => { return d.color; });
+      // add the text to the label group showing country name
+      countryLabels
+        .append("text")
+        .attr("class", "countryName")
+        .style("text-anchor", "middle")
+        .attr("dx", 0)
+        .attr("dy", 0)
+        .text((d: any) => d.properties.name)
+        .call(getTextBox);
+      // add a background rectangle the same size as the text
+      countryLabels
+        .insert("rect", "text")
+        .attr("class", "countryBg")
+        .attr("transform", (d: any) => {
+          return "translate(" + (d.bbox.x - 2) + "," + d.bbox.y + ")";
+        })
+        .attr("width", (d: any) => d.bbox.width + 4)
+        .attr("height", (d: any) => d.bbox.height);
 
-        //Add the SVG Text Element to the svgContainer
-        var text = this.svg.selectAll("text")
-                        .data(countries)
-                        .enter()
-                        .append("text");
+      // booted!
+      setTimeout(() => {
+        initiateZoom();
+      })
 
-        //Add SVG Text Element Attributes
-        var textLabels = text
-                  .attr("class", "country-label")
-                  // .attr("x", (d: any) => { return this.path.centroid(d)[0]; })
-                  // .attr("y", (d: any) => { return this.path.centroid(d)[1]; })
-                  .attr("transform", (d: any) => { return "translate(" + this.path.centroid(d) + ")"; })
-                  .text((d: any)=> { return d.name; })
-                  .attr("dx", (d: any) => "0.1em")
-                  .attr("dy", (d: any) =>"0.15em")
-                  .attr("font-family", "sans-serif")
-                  .attr("font-size", "10px")
-                  .attr("fill", "red");
-
-      // // tooltip
-      // var zoneTooltip = group.append("div").attr("class", "zoneTooltip"),
-      // infoLabel = group.append("div").attr("class", "infoLabel");
+      console.groupEnd();
     }
   }
 }
